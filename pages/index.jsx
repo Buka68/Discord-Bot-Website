9715c66f706a12fb7807293ff9bac8e1931a48a8
Buka68 <81566894+Buka68@users.noreply.github.com>
2025-05-15 16:50:37 +0200

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -10,10 +10,10 @@ export default function Index() {
       <img width="500" src="/img/bck.png" />
       </div>
         <p className="animateHeader text-4xl font-extrabold text-center text-white">
-          MusicMaker
+          baro2
         </p>
         <p className="animateHeader text-white text-opacity-50 text-center mt-5">
-       How about moderating your own server with MusicMaker? Create your own free world and create your own playlist, experience this pleasure with the best sounds. 
+       How about moderating your own server with baro1? Create your own free world and create your own playlist, experience this pleasure with the best sounds. 
 	  </p>
         <div className="animateHeader mt-10 flex flex-wrap items-center justify-center gap-x-4">
   
